09904767e9cd7ff6458fb26db173394445eef57f
Filippova Ekaterina <lwrow220816@gmail.com>
2022-11-20 05:30:17 +0300

    modify ServerApplication

--- client-server-example/server/src/ru/chat/ServerApplication.java
+++ client-server-example/server/src/ru/chat/ServerApplication.java
@@ -7,6 +7,10 @@ import java.net.Socket;
 public class ServerApplication {
 
     private static final int SERVER_PORT = 8081;
+
+    private static final String LULLABY = "\uD83C\uDF1F Twinkle, twinkle, little star,\uD83C\uDF1F\n ⭐ How I wonder what you are.⭐\nUp above the world so high,\n ✨ Like a diamond in the sky. ✨\n\uD83C\uDF1F Twinkle, twinkle, little star, \uD83C\uDF1F\n\uD83D\uDCAB  How I wonder what you are! \uD83D\uDCAB ";
+    private static final String CHRISTMAS_SONG = "❄️We wish you a merry Christmas❄\n❄️We wish you a merry Christmas❄\n❄️We wish you a merry Christmas and a happy new year!☃️\uD83C\uDF84 \uD83C\uDF85\uD83C\uDFFC";
+    private static final String BUNNY = "(\\__/)\n(='.'=)\n(\")_(\")";
     private static Socket clientSocket;
     private static ServerSocket serverSocket;
     private static BufferedReader input;
@@ -40,27 +44,22 @@ public class ServerApplication {
             System.out.printf("IOException with write a message \"%s\"", message);
         }
     }
+
     public static void choose(String message) {
             if (message.equals("sing") || message.equals("Sing") || message.equals("SING")) {
-                writeChosen("❄️We wish you a merry Christmas❄️\n" +
-                        "❄️We wish you a merry Christmas❄️\n" +
-                        "❄️We wish you a merry Christmas and a happy new year!☃️\uD83C\uDF84 \uD83C\uDF85\uD83C\uDFFC");
+                writeChosen(CHRISTMAS_SONG);
             } else if (message.equals("draw") || message.equals("Draw") || message.equals("DRAW")) {
-                writeChosen("(\\__/)\n(='.'=)\n(\")_(\")");
+                writeChosen(BUNNY);
             } else if (message.equals("bath") || message.equals("Bath") || message.equals("BATH")) {
                 writeChosen("Just kidding! \uD83D\uDE04 I'm not waterproof.");
             } else if (message.equals("sleep") || message.equals("Sleep") || message.equals("SLEEP")) {
                 writeChosen("I'll sing you a lullaby:");
-                lullaby();
+                writeChosen(LULLABY);
             } else if (message.equals("exit") || message.equals("Exit") || message.equals("EXIT")) {
                 writeChosen("Hope to see you later! Bye! \uD83D\uDC4B");
             } else {
                 writeChosen("\uD83D\uDE00I'm sorry, but I don't understand you. Please, try again.\uD83E\uDD17");
             }
-    }
-
-    public static void lullaby() {
-        writeChosen(String.format("\uD83C\uDF1F Twinkle, twinkle, little star,\uD83C\uDF1F\n ⭐ How I wonder what you are.⭐\nUp above the world so high,\n ✨ Like a diamond in the sky. ✨\n\uD83C\uDF1F Twinkle, twinkle, little star, \uD83C\uDF1F\n\uD83D\uDCAB  How I wonder what you are! \uD83D\uDCAB "));
     }
 
     public static void main(String[] args) {
@@ -79,6 +78,7 @@ public class ServerApplication {
 
                 writeChosen("Hello! I'm glad to see you here.");
                 writeChosen("I'm a little chat-bot Lili. And what is your name?");
+
                 message = input.readLine();
                 writer.write(String.format("Nice to meet you, %s!\n", message));
                 writer.flush();
