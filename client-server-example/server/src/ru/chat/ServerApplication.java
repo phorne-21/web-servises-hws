1e4b051e9aed7f745474dfece9ac509d56f6a550
Filippova Ekaterina <lwrow220816@gmail.com>
2022-11-20 17:56:54 +0300

    modified ServerApplication

--- client-server-example/server/src/ru/chat/ServerApplication.java
+++ client-server-example/server/src/ru/chat/ServerApplication.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class ServerApplication {
 
     private static final int SERVER_PORT = 8081;
-    private static final String LULLABY = "\uD83C\uDF1F Twinkle, twinkle, little star,\uD83C\uDF1F\n ⭐ How I wonder what you are.⭐\nUp above the world so high,\n ✨ Like a diamond in the sky. ✨\n\uD83C\uDF1F Twinkle, twinkle, little star, \uD83C\uDF1F\n\uD83D\uDCAB  How I wonder what you are! \uD83D\uDCAB ";
+    private static final String LULLABY = "\uD83C\uDF1F Twinkle, twinkle, little star,\uD83C\uDF1F\n ⭐ How I wonder what you are.⭐\n\uD83C\uDF1B Up above the world so high, \uD83D\uDCA4\n ✨ Like a diamond in the sky. ✨\n\uD83C\uDF1F Twinkle, twinkle, little star, \uD83C\uDF1F\n\uD83D\uDCAB  How I wonder what you are! \uD83D\uDCAB ";
     private static final String CHRISTMAS_SONG = "❄️We wish you a merry Christmas❄\n❄️We wish you a merry Christmas❄\n❄️We wish you a merry Christmas and a happy new year!☃️\uD83C\uDF84 \uD83C\uDF85\uD83C\uDFFC";
     private static final String BUNNY = "(\\__/)\n(='.'=)\n(\")_(\")";
 
@@ -15,7 +15,6 @@ public class ServerApplication {
     private static ServerSocket serverSocket;
     private static BufferedReader input;
     private static BufferedWriter writer;
-    private static String message;
 
     public static void showStartMenu() {
         try {
@@ -46,53 +45,53 @@ public class ServerApplication {
     }
 
     public static void choose(String message) {
-            if (message.equals("sing") || message.equals("Sing") || message.equals("SING")) {
-                writeChosen(CHRISTMAS_SONG);
-            } else if (message.equals("draw") || message.equals("Draw") || message.equals("DRAW")) {
-                writeChosen(BUNNY);
-            } else if (message.equals("bath") || message.equals("Bath") || message.equals("BATH")) {
-                writeChosen("Just kidding! \uD83D\uDE04 I'm not waterproof.");
-            } else if (message.equals("sleep") || message.equals("Sleep") || message.equals("SLEEP")) {
+        switch (message) {
+            case "sing", "Sing", "SING" -> writeChosen(CHRISTMAS_SONG);
+            case "draw", "Draw", "DRAW" -> writeChosen(BUNNY);
+            case "bath", "Bath", "BATH" -> writeChosen("Just kidding! \uD83D\uDE04 I'm not waterproof.");
+            case "sleep", "Sleep", "SLEEP" -> {
                 writeChosen("I'll sing you a lullaby:");
                 writeChosen(LULLABY);
-            } else if (message.equals("exit") || message.equals("Exit") || message.equals("EXIT")) {
-                writeChosen("Hope to see you later! Bye! \uD83D\uDC4B");
-            } else {
-                writeChosen("\uD83D\uDE00I'm sorry, but I don't understand you. Please, try again.\uD83E\uDD17");
             }
+            case "exit", "Exit", "EXIT" -> writeChosen("Hope to see you later! Bye! \uD83D\uDC4B");
+            default -> writeChosen("\uD83D\uDE00I'm sorry, but I don't understand you. Please, try again.\uD83E\uDD17");
+        }
     }
 
     public static void main(String[] args) {
         try {
             serverSocket = new ServerSocket(SERVER_PORT);
             System.out.println("Сервер запущен");
-            //для получения сообщений от клиента. accept() - ожидает соединение с клиентом
-            clientSocket = serverSocket.accept();
-            //поток выполнения тут блокируется до тех пор, пока сервер не полут ответ от клиента
-            System.out.println("Client started on port: " + clientSocket.getPort());
+            while (true) {
+                System.out.println("Server is waiting for client connection.");
+                //для получения сообщений от клиента. accept() - ожидает соединение с клиентом
+                clientSocket = serverSocket.accept();
+                //поток выполнения тут блокируется до тех пор, пока сервер не полут ответ от клиента
+                System.out.println("Client started on port: " + clientSocket.getPort());
 
-            try {
-                // получаем потоки с clientSocket
-                input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                try {
+                    // получаем потоки с clientSocket
+                    input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                writeChosen("Hello! I'm glad to see you here.");
-                writeChosen("I'm a little chat-bot Lili. And what is your name?");
+                    writeChosen("Hello! I'm glad to see you here.");
+                    writeChosen("I'm a little chat-bot Lili. And what is your name?");
 
-                message = input.readLine();
-                writer.write(String.format("Nice to meet you, %s!\n", message));
-                writer.flush();
+                    String message = input.readLine();
+                    writer.write(String.format("Nice to meet you, %s!\n", message));
+                    writer.flush();
 
-                do {
-                    showStartMenu();
-                    message = input.readLine();
-                    choose(message);
-                } while (!message.equals("exit"));
+                    do {
+                        showStartMenu();
+                        message = input.readLine();
+                        choose(message);
+                    } while (!message.equals("exit"));
 
-            } finally {
-                input.close();
-                writer.close();
-                clientSocket.close();
+                } finally {
+                    input.close();
+                    writer.close();
+                    clientSocket.close();
+                }
             }
         } catch (IOException e) {
             System.out.printf("IOException with create server application in port %s", SERVER_PORT);
